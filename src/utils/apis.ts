7eca147ae449d5ba0629535c65faee97f8c6d9fa
Stavros Kollias <kollias1252@gmail.com>
2020-11-02 21:18:58 +0000

    did initial Apis

---
+++ src/utils/apis.ts
@@ -0,0 +1,31 @@
+async function getRandomJoke(): Promise<any> {
+   try {
+      const response = await fetch(`http://api.icndb.com/jokes/random?exclude=[explicit]`);
+      const data = await response.json();
+      return data;
+   } catch (e) {
+      return null;
+   }
+}
+
+async function getRandomJokeByCharacter(firstName?: string, lastName?: string): Promise<any> {
+   try {
+      const response = await fetch(`http://api.icndb.com/jokes/random?exclude=[explicit]&firstName=${firstName}&${lastName}`);
+      const data = await response.json();
+      return data;
+   } catch (e) {
+      return null;
+   }
+}
+
+async function getMultipleRandomJokes(numberJokes: number): Promise<any> {
+   try {
+      const response = await fetch(`http://api.icndb.com/jokes/random/${numberJokes}`);
+      const data = await response.json();
+      return data;
+   } catch (e) {
+      return null;
+   }
+}
+
+export { getRandomJoke, getRandomJokeByCharacter, getMultipleRandomJokes };
